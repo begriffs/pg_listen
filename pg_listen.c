88c72e10d86bbdca8c58f38226a13b0de7f33791
andytango <andrew@rioja.io>
2020-01-14 15:49:51 +0000

    Reimplement logging changes

--- pg_listen.c
+++ pg_listen.c
@@ -1,9 +1,11 @@
 #include <errno.h>
 #include <libpq-fe.h>
 #include <poll.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <unistd.h>
 
 void		listen_forever_print(PGconn *, const char *);
@@ -12,6 +14,8 @@ void 		listen_poll(PGconn *, const char *);
 int			reset_if_necessary(PGconn *);
 void		clean_and_die(PGconn *);
 void		begin_listen(PGconn *, const char *);
+int			print_log_notify(PGnotify *);
+int			print_log(const char *, const char *, ...);
 
 #define		BUFSZ 512
 
@@ -24,8 +28,7 @@ main(int argc, char **argv)
 
 	if (argc < 4)
 	{
-		fprintf(stderr, "USAGE: %s db-url channel /path/to/program [args]\n",
-				argv[0]);
+		printf("USAGE: %s db-url channel /path/to/program [args]\n", argv[0]);
 		return EXIT_FAILURE;
 	}
 
@@ -46,7 +49,7 @@ main(int argc, char **argv)
 	prog = argv[3];
 	if(*prog == '-')
 	{
-		fprintf(stderr, "No executable given, sending notifications to stdout\n");
+		print_log("INFO", "No executable given, sending notifications to stdout");
 		listen_forever_print(conn, chan);
 	} else {
 		listen_forever_exec(conn, chan, prog, argv+3);
@@ -71,13 +74,8 @@ listen_forever_print(PGconn *conn, const char *chan)
 
 		while ((notify = PQnotifies(conn)) != NULL)
 		{
-			fprintf(stderr,
-					"NOTIFY of '%s' received from backend PID %d\n",
-					notify->relname, 
-					notify->be_pid
-			);
-
-			printf("%s\n", notify->extra);
+			print_log_notify(notify);
+			printf("%s", notify->extra);
 
 			PQfreemem(notify);
 		}
@@ -98,11 +96,7 @@ listen_forever_exec(PGconn *conn, const char *chan, const char *cmd, char **args
 
 		while ((notify = PQnotifies(conn)) != NULL)
 		{
-			fprintf(stderr,
-					"NOTIFY of '%s' received from backend PID %d\n",
-					notify->relname, 
-					notify->be_pid
-			);
+			print_log_notify(notify);
 			/* we'll send NOTIFY payload through pipe to stdin */
 			if (errno = 0, pipe(pipefds) < 0)
 			{
@@ -132,7 +126,7 @@ listen_forever_exec(PGconn *conn, const char *chan, const char *cmd, char **args
 					}
 					if (errno = 0, execv(cmd, args) < 0)
 					{
-						fprintf(stderr, "Can't run %s: %s\n",
+						print_log("CRITICAL", "Can't run %s: %s",
 						        cmd, strerror(errno));
 						close(pipefds[0]);
 						exit(EXIT_FAILURE);
@@ -163,7 +157,7 @@ listen_poll(PGconn *conn, const char *chan)
 	sock = PQsocket(conn);
 	if (sock < 0)
 	{
-		fprintf(stderr, "Failed to get libpq socket: %s\n",
+		print_log("CRITICAL", "Failed to get libpq socket: %s",
 				PQerrorMessage(conn));
 		clean_and_die(conn);
 	}
@@ -193,15 +187,15 @@ reset_if_necessary(PGconn *conn)
 			seconds = 1;
 		else
 		{
-			fprintf(stderr, "Failed.\nSleeping %d seconds.\n", seconds);
+			print_log("ERROR", "Failed.\nSleeping %d seconds.", seconds);
 			sleep(seconds);
 			seconds *= 2;
 		}
-		fprintf(stderr, "Reconnecting to database...");
+		print_log("INFO", "Reconnecting to database...");
 		PQreset(conn);
 	} while (PQstatus(conn) != CONNECTION_OK);
 
-	fprintf(stderr, "Connected.\n");
+	print_log("INFO", "Connected.");
 	return 1;
 }
 
@@ -211,14 +205,15 @@ begin_listen(PGconn *conn, const char *chan)
 	PGresult   *res;
 	char		sql[7 + BUFSZ + 1];
 
-	fprintf(stderr, "Listening for channel %s\n", chan);
+
+	print_log("INFO", "Listening for channel %s", chan);
 
 	snprintf(sql, 7 + BUFSZ + 1, "LISTEN %s", chan);
 	res = PQexec(conn, sql);
 
 	if (PQresultStatus(res) != PGRES_COMMAND_OK)
 	{
-		fprintf(stderr, "LISTEN command failed: %s\n", PQerrorMessage(conn));
+		print_log("CRITICAL", "LISTEN command failed: %s", PQerrorMessage(conn));
 		PQclear(res);
 		clean_and_die(conn);
 	}
@@ -230,4 +225,32 @@ clean_and_die(PGconn *conn)
 {
 	PQfinish(conn);
 	exit(EXIT_FAILURE);
+}
+
+int
+print_log_notify(PGnotify *notify)
+{
+	return print_log("INFO",
+					 "NOTIFY of '%s' received from backend PID %d",
+					 notify->relname, 
+					 notify->be_pid
+					);
+}
+
+int
+print_log(const char *sev, const char *fmt, ...)
+{
+	va_list	args;
+	time_t	now = time(NULL);
+	char	timestamp[128];
+	int		res;
+
+	strftime(timestamp, sizeof timestamp,"%Y-%m-%dT%H:%M:%S", gmtime(&now));
+	res = fprintf(stderr, "%s - pg_listen - %s - ", timestamp, sev);
+
+	va_start(args, fmt);
+	res = res + vfprintf(stderr, fmt, args);
+	va_end(args);
+
+	return  res + fprintf(stderr, "\n");
 }
